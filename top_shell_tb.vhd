a2f8a0d63775ef476c8b0ccc307c33f98b65ebeb
tommyerwin7 <tommyerwin7@gmail.com>
2025-06-05 12:09:11 -0400

    Update top_shell_tb.vhd

--- top_shell_tb.vhd
+++ top_shell_tb.vhd
@@ -1,58 +1,78 @@
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-ENTITY top_shell_tb is
-END top_shell_tb;
+entity top_shell_tb is
+end top_shell_tb;
 
 architecture behavior of top_shell_tb is
 
-component top_level
-PORT (  clk         : in std_logic;
-        Parallel_in : in  std_logic_vector(7 downto 0);
-        MOSI        : out std_logic;
-        SCLK        : out std_logic;
-        CS          : out std_logic):
-END COMPONENT; 
+    component top_shell
+        port (
+            clk         : in std_logic;
+            Parallel_in : in std_logic_vector(7 downto 0);
+            MISO        : in std_logic;
+            MOSI        : out std_logic;
+            SCLK        : out std_logic;
+            CS          : out std_logic
+        );
+    end component;
 
-SIGNAL clk         : std_logic;
-SIGNAL Parallel_in : std_logic_vector(7 downto 0);
-SIGNAL MOSI        : std_logic;
-SIGNAL SCLK        : std_logic;
-SIGNAL CS          : std_logic;
+    -- signals
+    signal clk         : std_logic := '0';
+    signal Parallel_in : std_logic_vector(7 downto 0) := (others => '0');
+    signal MISO        : std_logic := '0';
+    signal MOSI        : std_logic;
+    signal SCLK        : std_logic;
+    signal CS          : std_logic;
 
-constant clk_period : time := 10 ns; --10 MHz clock
+    constant clk_period : time := 10 ns;
 
-uut:  top_level port map( 
-    clk         => clk,
-    Parallel_in => Parallel_in,
-    MOSI        => MOSI,
-    SCLK        => SCLK,
-    CS          => CS ); 
-
-
-clk_proc : process
 begin
-	  clk <= '0';
-	  wait for clk_period/2;
-	  clk <= '1';
-	  wait for clk_period/2;
-end process;
 
+    -- DUT instantiation
+    uut: top_shell
+        port map (
+            clk         => clk,
+            Parallel_in => Parallel_in,
+            MISO        => MISO,
+            MOSI        => MOSI,
+            SCLK        => SCLK,
+            CS          => CS
+        );
 
-stim_proc : process
-begin	
-    wait for 20ns;
+    -- Clock process
+    clk_process : process
+    begin
+        while now < 500 ns loop  -- Run sim for 500 ns max
+            clk <= '0';
+            wait for clk_period / 2;
+            clk <= '1';
+            wait for clk_period / 2;
+        end loop;
+        wait;
+    end process;
 
-    parallel_in => "10011001"
-    wait for 2 * clk_period;
+    -- Stimulus process
+    stim_proc : process
+    begin
+        -- Wait for reset/setup
+        wait for 20 ns;
 
-    parallel_in => "11111000"
-    wait for 2 * clk_period;
+        -- Send 3 bytes
+        Parallel_in <= "10011001";
+        wait for clk_period * 2;
 
-    parallel_in => "00011011"
-    wait for 2 * clk_period;
+        Parallel_in <= "11111000";
+        wait for clk_period * 2;
 
-    wait; 
-end process stim_proc; 
-end behavior; 
+        Parallel_in <= "00011011";
+        wait for clk_period * 2;
+
+        -- Idle
+        wait for 200 ns;
+
+        wait;
+    end process;
+
+end behavior;
