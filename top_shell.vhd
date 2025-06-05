99ec42ae52b36a6cc3df8ec0c029af347cc4eb11
tommyerwin7 <tommyerwin7@gmail.com>
2025-06-05 12:08:25 -0400

    Update top_shell.vhd

--- top_shell.vhd
+++ top_shell.vhd
@@ -4,15 +4,19 @@ use IEEE.numeric_std.all;
 
 entity top_shell is
     port (
-        clk  : in std_logic;
-     
+        clk         : in std_logic;
+        Parallel_in : in std_logic_vector(7 downto 0);
+        MISO        : in std_logic;
+        MOSI        : out std_logic;
+        SCLK        : out std_logic;
+        CS          : out std_logic
     );
 end top_shell;
 
 architecture behavioral_architecture of top_shell is
 
     -- COMPONENT DECLARATIONS
-    COMPONENT SCI_receiver
+    component SCI_receiver
         port (
             clk         : in  std_logic;
             Parallel_in : in  std_logic_vector(7 downto 0);
@@ -20,30 +24,30 @@ architecture behavioral_architecture of top_shell is
             Tx          : out std_logic;
             Tx_done     : out std_logic
         );
-    end COMPONENT;
+    end component;
 
-    COMPONENT SCI_processor
+    component SCI_processor
         port (
             clk       : in  std_logic;
-            new_data  : in  std_logic; 
+            new_data  : in  std_logic;
             data_in   : in  std_logic_vector(7 downto 0);
             midi_chn  : out std_logic_vector(3 downto 0);
             power_on  : out std_logic_vector(3 downto 0);
             tone_out  : out std_logic_vector(7 downto 0);
             vel_out   : out std_logic_vector(7 downto 0)
         );
-    end COMPONENT;
+    end component;
 
-    COMPONENT DDS
+    component DDS
         port (
-            clk       : in  std_logic;
-            tone_sig  : in  std_logic_vector(7 downto 0);
-            sin_sig   : out std_logic_vector(15 downto 0);
-	    data_valid: out std_logic
+            clk        : in  std_logic;
+            tone_sig   : in  std_logic_vector(7 downto 0);
+            sin_sig    : out std_logic_vector(15 downto 0);
+            data_valid : out std_logic
         );
-    end COMPONENT;
+    end component;
 
-    COMPONENT SPI_tx
+    component SPI_tx
         port (
             clk         : in  std_logic;
             Parallel_in : in  std_logic_vector(11 downto 0);
@@ -54,11 +58,10 @@ architecture behavioral_architecture of top_shell is
             CS          : out std_logic;
             Tx_done     : out std_logic
         );
-    end COMPONENT;
+    end component;
 
-    -- SIGNAL DECLARATIONS
-    signal byte_in       : std_logic_vector(7 downto 0);
-    signal load_in       : std_logic;
+    -- INTERNAL SIGNALS
+    signal load_in       : std_logic := '1';  -- simplified always-high (optional control logic)
     signal done_in       : std_logic;
 
     signal midi_channel  : std_logic_vector(3 downto 0);
@@ -67,45 +70,50 @@ architecture behavioral_architecture of top_shell is
     signal velocity      : std_logic_vector(7 downto 0);
 
     signal sine_wave     : std_logic_vector(15 downto 0);
-    signal spi_start     : std_logic;
+    signal data_valid    : std_logic; -- from DDS
     signal spi_done      : std_logic;
+    
 
 begin
 
-    -- COMPONENT INSTANTIATIONS
 
+    -- Connect SCI_receiver
     SCI_rec_tl: SCI_receiver
         port map (
             clk         => clk,
-            Parallel_in => byte_in,
-            Load        => load_in,
+            Parallel_in => Parallel_in,   -- feed directly from input
+            Load        => load_in,       -- trigger for reception
             Tx          => open,
             Tx_done     => done_in
         );
 
+    -- Connect SCI_processor
     SCI_pro_tl: SCI_processor
         port map (
             clk       => clk,
-            new_data  => done_in,
-            data_in   => byte_in,
+            new_data  => done_in,         -- pulse from SCI_receiver
+            data_in   => Parallel_in,     -- same input passed down
             midi_chn  => midi_channel,
             power_on  => power_sig,
             tone_out  => tone_byte,
             vel_out   => velocity
         );
 
+    -- Connect DDS
     DDS_tl: DDS
         port map (
-            clk      => clk,
-            tone_sig => tone_byte,
-            sin_sig  => sine_wave
+            clk        => clk,
+            tone_sig   => tone_byte,
+            sin_sig    => sine_wave,
+            data_valid => data_valid      -- drives SPI trigger
         );
 
+    -- Connect SPI_tx
     SPI_tl: SPI_tx
         port map (
             clk         => clk,
-            Parallel_in => sine_wave(15 downto 4),  -- take upper 12 bits
-            New_data    => spi_start,
+            Parallel_in => sine_wave(15 downto 4),  -- upper 12 bits
+            New_data    => data_valid,              -- trigger from DDS
             MISO        => MISO,
             MOSI        => MOSI,
             SCLK        => SCLK,
