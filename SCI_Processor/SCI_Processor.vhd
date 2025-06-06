62cbbbc5ebf49382bf04d28c0d9c412b5b532b77
Asa Chard <asa.a.chard.27@dartmouth.edu>
2025-06-06 15:24:27 -0400

    Update SCI_Processor.vhd

--- SCI_Processor/SCI_Processor.vhd
+++ SCI_Processor/SCI_Processor.vhd
@@ -10,7 +10,24 @@ PORT ( 	clk			: in	std_logic;
         midi_chn	: out	std_logic_vector(3 downto 0);
         power_on	: out	std_logic_vector(3 downto 0);
         tone_out 	: out	std_logic_vector(7 downto 0);
-        vel_out		: out	std_logic_vector(7 downto 0));
+        vel_out		: out	std_logic_vector(7 downto 0)
+        --light_16    : out std_logic;--16 downto 9 will be the tone 
+       -- light_15    : out std_logic;
+       -- light_14    : out std_logic;
+       -- light_13    : out std_logic;
+      --  light_12    : out std_logic; 
+      --  light_11    : out std_logic; 
+      --  light_10    : out std_logic; 
+    --    light_9    : out std_logic; 
+       -- light_8   : out std_logic; --8 downto 4 will be the power
+     --   light_7   : out std_logic;
+     --   light_6   : out std_logic;
+      --  light_5   : out std_logic;
+     --   light_4   : out std_logic; --3 downto 1 is channel
+      --  light_3   : out std_logic;
+     --   light_2   : out std_logic;
+      --  light_1   : out std_logic     
+        );
 end SCI_Processor;
         
 architecture behavior of SCI_Processor is
@@ -24,11 +41,13 @@ signal channel		: std_logic_vector(3 downto 0);
 signal tone			: std_logic_vector(7 downto 0);
 signal velocity		: std_logic_vector(7 downto 0);
 
-signal Power_en	:	std_logic := '0';
-signal tone_en	:	std_logic := '0';
-signal vel_en	:	std_logic := '0';
+signal update_on_reg : std_logic  := '0';
+signal update_tone_reg : std_logic  := '0';
+signal update_velocity_reg : std_logic  := '0';
 
 BEGIN
+
+
 
 state_update : process (clk)
 begin
@@ -65,28 +84,50 @@ end process next_state_logic;
 
 out_logic : process (cs)
 begin
-
+   update_on_reg <= '0';
+   update_tone_reg <= '0';
+   update_velocity_reg <= '0';
+   
 	case cs is
     
     	when idle => 
+
         
         when on_receive =>
-        	power <= data_in(7 downto 4);
-            channel <= data_in(3 downto 0);
+            update_on_reg <= '1';
+        	
             
         when tone_receive =>
-        	tone <= data_in;
+        	update_tone_reg <= '1';
         
         when vel_receive =>
-			velocity <= data_in;
+			update_velocity_reg <= '1';
             
     end case; 
 
 end process out_logic; 
 
+update_registers : process(clk)
+begin
+    if rising_edge(clk) then
+        if update_on_reg = '1' then
+            power <= data_in(7 downto 4);
+            channel <= data_in(3 downto 0);
+        end if;
+        if update_tone_reg = '1' then
+            tone <= data_in;
+        end if;
+        if update_velocity_reg = '1' then
+            velocity <= data_in;
+        end if;
+    end if;
+end process update_registers;
+
 power_on <= power;
 midi_chn <= channel;
 tone_out <= tone;
 vel_out <= velocity;
+
+
 
 end behavior;
